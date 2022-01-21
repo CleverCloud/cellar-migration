f1dce0e79cfa5d7c0456b464d20106e9c7d469d0
Arnaud Lefebvre <arnaud.lefebvre@clever-cloud.com>
2022-01-21 20:20:15 +0100

    CI: update rust-build.actions to 1.2.6

--- pre-build.sh
+++ pre-build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-# Script until https://github.com/rust-build/rust-build.action
-# gets a new release
-echo "Running pre-build script"
-sed '/^export CXX.*/a export PATH="/opt/osxcross/target/bin:$PATH"\nexport LIBZ_SYS_STATIC=1' -i /build.sh
-
 echo "Installing needed dependencies"
 apk add make perl gcc

5bc2f05b591f48de20f42733cbb36d948f340c9d
Arnaud Lefebvre <arnaud.lefebvre@clever-cloud.com>
2023-01-17 16:26:06 +0100

    Add basic HTTP Server to ease deployment in Clever Cloud application

---
+++ src/http-server.rs
@@ -0,0 +1,30 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+// Copied from Hyper tutorial
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Clever Cloud S3 Migration Tool".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let port = std::env::var("PORT").unwrap_or("8080".to_string());
+    let addr = SocketAddr::from((
+        [0, 0, 0, 0],
+        port.parse::<u16>().expect("Port variable should be a u16"),
+    ));
+
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
